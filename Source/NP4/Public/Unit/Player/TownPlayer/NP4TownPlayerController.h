afa1a6aace76d9234cf9439125d3f4c96106127d
Junghoon <Junghoon@webzen.com>
2016-02-13 17:05:35 +0900

    마을용 플레이어 컨트롤러 추가

---
+++ Source/NP4/Public/Unit/Player/TownPlayer/NP4TownPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Unit/Player/NP4PlayerController.h"
+#include "NP4TownPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NP4_API ANP4TownPlayerController : public ANP4PlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
