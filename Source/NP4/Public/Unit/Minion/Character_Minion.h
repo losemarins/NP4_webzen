64927e0e8c75d5917bf7c14d4204a835e4528830
KooYoung <foreverikazi@naver.com>
2016-02-16 14:53:07 +0900

    미니언 코드 추가

--- Source/NP4/Public/Unit/Minion/Character_Minion.h
+++ Source/NP4/Public/Unit/Minion/Character_Minion.h
@@ -12,13 +12,14 @@ UCLASS()
 class NP4_API ACharacter_Minion : public ANP4CharacterBase
 {
 	GENERATED_BODY()
-	
+	class UPawnSensingComponent* PawnSensingComp;
+
 public :
 	ACharacter_Minion();
 	void UpdatePawnData(); // 나중에 base에서 virtual로 할수도있음
+	UFUNCTION()
 	void OnSeeEnemy(APawn* Pawn);
 	
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
-	
 };
