93d58c5a479c8fb7d62856bf28a5930f42b210bf
KooYoung <foreverikazi@naver.com>
2016-02-18 18:20:23 +0900

    no message

---
+++ enc_temp_folder/1dd28236decc59685955aec4dcc527/NP4HeroMeleeAttackNotify.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "NP4PlayerBase.h"
+#include "NP4HeroMeleeAttackNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NP4_API UNP4HeroMeleeAttackNotify : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+private:
+	eCombo_Interpol m_eCurrentComboNum;
+	bool m_bNextSectionReady;
+
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) override;
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) override;
+	
+	
+};
